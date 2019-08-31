63a5e452c1ed7b682dd8c680a8846d16a75417bd
Xrysnow <xrysnow@outlook.com>
2019-09-01 00:47:42 +0800

    doc work for RecordingDevice

--- AudioRecordingDevice.h
+++ AudioRecordingDevice.h
@@ -7,6 +7,9 @@
 
 namespace audio
 {
+	/**
+	 * Represents an audio input device capable of recording sounds.
+	 */
 	class RecordingDevice : public cocos2d::Ref
 	{
 	public:
@@ -23,59 +26,73 @@ namespace audio
 		virtual ~RecordingDevice();
 
 		/**
-		 * Begins audio input recording process.
-		 * @param samples Number of samples to buffer.
-		 * @param sampleRate Desired sample rate.
-		 * @param bitDepth Desired bit depth (8 or 16).
-		 * @param channels Desired number of channels.
-		 * @return True if recording started successfully.
-		 **/
+		 * Begins recording audio using this device.
+		 * @note A ring buffer is used internally to store recorded data before RecordingDevice:getData or
+		 * RecordingDevice:stop are called – the former clears the buffer. If the buffer completely fills up
+		 * before getData or stop are called, the oldest data that doesn't fit into the buffer will be lost.
+		 * @param samples The maximum number of samples to store in an internal ring buffer when recording.
+		 * RecordingDevice:getData clears the internal buffer when called.
+		 * @param sampleRate The number of samples per second to store when recording.
+		 * @param bitDepth The number of bits per sample (8 or 16).
+		 * @param channels Whether to record in mono or stereo. Most microphones don't support more than 1 channel.
+		 * @return True if the device successfully began recording using the specified parameters, false if not.
+		 */
 		virtual bool start(int samples, int sampleRate, int bitDepth, int channels);
 
 		/**
-		 * Stops audio input recording.
-		 **/
+		 * Stops recording audio from this device. 
+		 */
 		virtual void stop();
 
 		/**
-		 * Retreives recorded data.
-		 * @return SoundData containing data obtained from recording device.
-		 **/
+		 * Gets all recorded audio SoundData stored in the device's internal ring buffer.
+		 * 
+		 * The internal ring buffer is cleared when this function is called, so calling it again will only
+		 * get audio recorded after the previous call. If the device's internal ring buffer completely fills up
+		 * before getData is called, the oldest data that doesn't fit into the buffer will be lost.
+		 * @return The recorded audio data, or null if the device isn't recording.
+		 */
 		virtual SoundData* getData();
 
 		/**
-		 * @return device name.
-		 **/
+		 * Gets the name of the recording device.
+		 * @return The name of the device.
+		 */
 		virtual std::string getName() const;
 
 		/**
-		 * @return Number of samples currently recorded.
-		 **/
+		 * Gets the number of currently recorded samples.
+		 * @return The number of samples that have been recorded so far.
+		 */
 		virtual int getSampleCount() const;
 
 		/**
 		 * Gets the maximum number of samples that will be buffered, as set by start().
-		 **/
+		 */
 		virtual int getMaxSamples() const;
 
 		/**
-		 * @return Sample rate for recording.
-		 **/
+		 * Gets the number of samples per second currently being recorded.
+		 * @return The number of samples being recorded per second (sample rate).
+		 */
 		virtual int getSampleRate() const;
 
 		/**
-		 * @return Bit depth for recording.
-		 **/
+		 * Gets the number of bits per sample in the data currently being recorded.
+		 * @return The number of bits per sample in the data that's currently being recorded.
+		 */
 		virtual int getBitDepth() const;
 
 		/**
-		 * @return Number of channels for recording.
-		 **/
+		 * Gets the number of channels currently being recorded (mono or stereo).
+		 * @return The number of channels being recorded (1 for mono, 2 for stereo).
+		 */
 		virtual int getChannelCount() const;
 
 		/**
-		 * @return True if currently recording.
-		 **/
+		 * Gets whether the device is currently recording.
+		 * @return True if the RecordingDevice is recording, false otherwise.
+		 */
 		virtual bool isRecording() const;
 
 	private:
